07a8820ecdb27d279fed9798fa6a0b968d9fa4b5
4rya-git <4rya.git@gmail.com>
2024-07-18 18:17:09 +0530

    add tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jwt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -43,6 +45,49 @@ const userSchema = new mongoose.Schema({
         type: String,
     },
 }, { timestamps: true });
+
+
+
+useSchema.pre('save', async function (next) {
+
+    if (!this.modified("password")) {
+        return next();
+    }
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+};
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    );
+};
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        },
+    );
+};
+
 
 const userModel = mongoose.model('users', userSchema, 'users');
 
